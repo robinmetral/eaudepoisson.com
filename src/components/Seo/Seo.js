9f88abf084a54b64a5258bb9ec18dbbd890b1fea
Robin Métral <robin@metral.ch>
2020-03-29 17:41:51 +0200

    fix: fix SEO low-hanging fruits (#53)
    * fix: switch default lang prop to fr
    
    * feat: add site description and add to default title
    
    Will be shown in the homepage title tag

--- src/components/Seo/Seo.js
+++ src/components/Seo/Seo.js
@@ -1,10 +1,3 @@
-/**
- * SEO component that queries for data with
- *  Gatsby's useStaticQuery React hook
- *
- * See: https://www.gatsbyjs.org/docs/use-static-query/
- */
-
 import React from "react"
 import PropTypes from "prop-types"
 import Helmet from "react-helmet"
@@ -34,7 +27,7 @@ function Seo({ description, lang, meta, title }) {
       }}
       title={title}
       titleTemplate={`%s | ${site.siteMetadata.title}`}
-      defaultTitle={site.siteMetadata.title}
+      defaultTitle={`${site.siteMetadata.title} | ${metaDescription}`}
       meta={[
         {
           name: `description`,
@@ -74,9 +67,9 @@ function Seo({ description, lang, meta, title }) {
 }
 
 Seo.defaultProps = {
-  lang: `en`,
+  lang: `fr`,
   meta: [],
-  description: ``,
+  description: `Clara & Robin en Asie du Sud-Est`,
 }
 
 Seo.propTypes = {
